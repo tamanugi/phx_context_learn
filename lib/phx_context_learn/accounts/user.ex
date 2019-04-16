c6eeddd6e4f680ee89cbc276fe7bdf9d620fa1f4
tamanugi <tamanugi@gmail.com>
2019-04-16 22:48:17 +0900

    👍 relationship user-credential

--- lib/phx_context_learn/accounts/user.ex
+++ lib/phx_context_learn/accounts/user.ex
@@ -1,10 +1,12 @@
 defmodule PhxContextLearn.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias PhxContextLearn.Accounts.Credential
 
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
